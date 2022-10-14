6a312846b5da07d68a18fdf22477a6147aa64e29
Дарья Файтельсон <daryafaitelson@gmail.com>
2022-10-14 15:09:19 +0300

    'code_refactored'

--- script.js
+++ script.js
@@ -1,65 +1,49 @@
 const listParent = document.querySelector(".card-list"),
     paginationParent = document.querySelector(".pagination__list");
-let paginatedItems,
-    itemsPerPage = 2;
 
 // classes
-class Link {
-    constructor(index, parent) {
-        this.index = index;
+class Elem {
+    constructor(parent, className, title, date, description) {
         this.parent = parent;
-    }
-
-    render() {
-        let link = document.createElement('a');
-        link.classList.add('pagination__link');
-        link.innerHTML = this.index;
-        this.parent.append(link);
-    }
-}
-
-class Card {
-    constructor(title, date, description, parent) {
+        this.className = className;
         this.title = title;
         this.date = date;
         this.description = description;
-        this.parent = parent;
+        this.inner = `
+             <h2 class="card__title">${this.title}</h2>
+             <h2 class="card__date">${this.date}</h2>
+             <div class="card__description">${this.description}</div>
+         `;
     }
 
     render() {
-        let listItem = document.createElement('div');
-        listItem.classList.add('card');
-        listItem.innerHTML = `
-            <h2 class="card__title">${this.title}</h2>
-            <h2 class="card__date">${this.date}</h2>
-            <div class="card__description">${this.description}</div>
-        `;
-        this.parent.append(listItem);
+        let elem = document.createElement('div');
+        elem.classList.add(this.className);
+        elem.innerHTML = this.inner;
+        this.parent.append(elem);
     }
 }
 
-// fetch
-fetch(`http://candidate.scid.ru/api/books`)
-    .then(response => response.json())
-    .then(data => {
-        for (let i = 1; i < data.result.links.length - 1; i++) {
-            new Link(i, paginationParent).render();
-        }
-        displayCards(data.result.data);
-        const links = document.querySelectorAll(".pagination__link");
-        links.forEach((link, index) => {
-            link.addEventListener('click', function (evt) {
-                evt.preventDefault();
-                fetchPages(index);
-            });
-        })
-    });
+class Link extends Elem {
+    constructor(parent, className, inner) {
+        super(parent, className);
+        this.parent = parent;
+        this.className = className;
+        this.inner = inner;
+    }
+}
 
 // callbacks
+function displayLinks(array) {
+    for (let i = 1; i < array.length - 1; i++) {
+        new Link(paginationParent, 'pagination__link', i).render();
+    }
+}
+
 function displayCards(array) {
     listParent.innerHTML = '';
     for (let i = 0; i < array.length; i++) {
-        new Card(array[i].name, array[i].date, array[i].description, listParent).render();
+        new Elem(listParent, 'card', array[i].name, array[i].date, array[i].description).render();
     }
 }
 
@@ -71,4 +55,16 @@ function fetchPages(i) {
         });
 }
 
-
+// fetch
+fetch(`http://candidate.scid.ru/api/books`)
+    .then(response => response.json())
+    .then(data => {
+        displayLinks(data.result.links);
+        displayCards(data.result.data);
+        const links = document.querySelectorAll(".pagination__link");
+        links.forEach((link, index) => {
+            link.addEventListener('click', function (evt) {
+                fetchPages(index);
+            });
+        })
+    });
